d83895ef9d74925d18c1c22a38ac97e3ee5fcf9e
Vijaya Raj M <vijayaraj@freshdesk.com>
2015-09-13 16:32:59 +0530

    Typo in QuestionSet model

--- app/models/question_set.rb
+++ app/models/question_set.rb
@@ -11,7 +11,7 @@ class QuestionSet < ActiveRecord::Base
   scope :approved, -> { where("approved is true") }
   scope :not_self, -> (user_id) { where("user_id <> #{user_id}") }
   
-  scope :filter_by_category, -> (category_id) { where("level_id = ?", category_id) }
+  scope :filter_by_category, -> (category_id) { where("category_id = ?", category_id) }
   scope :filter_by_level, -> (level_id) { where("level_id = ?", level_id) }
   scope :filter_by_category_and_level, -> (category_id, level_id) do
     where('category_id = ? and level_id = ?', category_id, level_id)
